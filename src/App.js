8cc0ac62c186dedc798dfb26ee415e5937a076fa
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-22 18:19:57 +0300

    refactor: refactor application with prettier

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ import { About } from './pages/About';
 import { Articles } from './pages/Articles';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<h1>Server-Side Rendering Example</h1>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/articles">Articles</Link>
-				</li>
-			</ul>
-			<Switch>
-				<Route path="/" exact>
-					<Home />
-				</Route>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/articles">
-					<Articles />
-				</Route>
-			</Switch>
-		</BrowserRouter>
-	);
-}
+  return (
+    <BrowserRouter>
+      <h1>Server-Side Rendering Example</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export const About = () => {
-	return (
-		<h1>About</h1>
-	);
-}
+  return <h1>About</h1>;
+};
