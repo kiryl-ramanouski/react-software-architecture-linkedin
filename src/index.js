b6afb256cd4903d57419fb6fe62ed512ddfbd9a5
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-23 12:05:19 +0300

    feat: replace BrowserRouter from App to index.js. Replace render() with hydrate()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// hydrate takes pre-rendered HTML that we're going to get from the server and adds React to it
+// Despite of we're getting an HTML from the server, we still want to behave and rerender like ReactApp
+ReactDOM.hydrate(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
