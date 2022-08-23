f58852872fdd2fcae3ecf73f6957689a9ef02e05
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-23 14:31:06 +0300

    feat: add ServerStyleSheet to server to be able to use styled-components

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ import path from 'path';
 import fs from 'fs';
 import App from './src/App';
 
+// Styles
+import { ServerStyleSheet } from 'styled-components';
+
 const app = express();
 
 app.use(express.static('./build', { index: false }));
