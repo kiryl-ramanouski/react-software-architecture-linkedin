1ad147c45fabb2f140569d7da485441378501a2f
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-24 12:38:36 +0300

    feat: add recoil selector to application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { RecoilRoot } from 'recoil';
 import { CounterButton } from './CounterButton';
-import { DisplayCount } from './DisplayCount';
 import './App.css';
 
 const App = () => {
   return (
     <RecoilRoot>
-      <DisplayCount />
       <h1>State Management Example</h1>
       <CounterButton />
     </RecoilRoot>
--- src/DisplayCount.js
+++ src/DisplayCount.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { counterState } from './counterState';
 
 export const DisplayCount = () => {
   const clicksData = useRecoilValue(counterState);
 
-  return <h2>Number of clicks is {clicksData.length}.</h2>;
+  return <h2>Number Of Clicks: {clicksData}</h2>;
 };
