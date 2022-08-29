e46d9fb64de0919a7b1725cda3eb513d09789238
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-29 12:13:56 +0300

    refactor: prepare application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import { Provider } from 'react-redux';
-import store from './store';
+import React from 'react';
+import styled from 'styled-components';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Articles } from './pages/Articles';
 
-import { CounterButton } from './CounterButton';
-
-import './App.css';
+const BigGreenHeading = styled.h1`
+  color: green;
+  font-size: 96px;
+`;
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <h1>State Management Example</h1>
-      <CounterButton />
-    </Provider>
+    <>
+      <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
--- src/selectors.js
+++
@@ -1,3 +0,0 @@
-export const getNumberOfClicks = (state) => {
-  return state.numberOfClicks;
-};
