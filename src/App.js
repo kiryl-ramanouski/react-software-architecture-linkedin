4387a0585db8c00c0afae92c48e683d478bc3c59
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-29 09:31:12 +0300

    feat: add store to application using { Provider } from 'react-redux'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
 import { CounterButton } from './CounterButton';
+
 import './App.css';
 
 const App = () => {
   return (
-    <>
+    <Provider store={configureStore}>
       <h1>State Management Example</h1>
       <CounterButton />
-    </>
+    </Provider>
   );
 };
 
