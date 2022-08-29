badec54ee02acfd21d80dd2f9864b5ea4d7d9c88
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-29 12:45:48 +0300

    refactor: prepare app

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ import { About } from './pages/About';
 import { Articles } from './pages/Articles';
 
 const BigGreenHeading = styled.h1`
-  color: green;
-  font-size: 96px;
+	color: green;
+	font-size: 96px;
 `;
 
 const App = () => {
-  return (
-    <>
-      <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/articles">Articles</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/articles">
-          <Articles />
-        </Route>
-      </Switch>
-    </>
-  );
-};
+	return (
+		<>
+		<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+		<ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/about">About</Link>
+			</li>
+			<li>
+				<Link to="/articles">Articles</Link>
+			</li>
+		</ul>
+		<Switch>
+			<Route path="/" exact>
+				<Home />
+			</Route>
+			<Route path="/about">
+				<About />
+			</Route>
+			<Route path="/articles">
+				<Articles />
+			</Route>
+		</Switch>
+		</>
+	);
+}
 
 export default App;
