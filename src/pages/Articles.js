15d7cfcaf16644b4e49b43f751f3e8fcc75f2a26
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-08-29 16:52:02 +0300

    feat: add articles using useState and useEffect with fetch and async IIEF

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Articles = () => {
-  return <h1>Articles</h1>;
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    return (async () => {
+      const response = await fetch('/api/articles');
+      const data = await response.json();
+      setArticles(data);
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles && (
+        <ul>
+          {articles.map((item, i) => {
+            return (
+              <li key={i}>
+                <p>{item.title}</p>
+                <p>{item.author}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
