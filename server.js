55e5c1fcfa70b00a07b9ba11a7a913431864aa1d
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-08-30 17:33:56 +0300

    feat: update server with context. Add Provider with Promise.all(contextObj._requests)

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
+// Context
+import { InitialDataContext } from './src/InitialDataContext';
+
 // Components
 import App from './src/App';
 
@@ -33,15 +36,31 @@ app.get('/api/articles', (req, res) => {
   res.json(loadedArticles);
 });
 
-app.get('/*', (req, res) => {
+app.get('/*', async (req, res) => {
   const sheet = new ServerStyleSheet();
+  const Provider = InitialDataContext.Provider;
+  const contextObj = { _isServerSide: true, _requests: [], _data: {} };
+
+  renderToString(
+    sheet.collectStyles(
+      <Provider value={contextObj}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    )
+  );
+
+  await Promise.all(contextObj._requests);
+  contextObj._isServerSide = false;
+  delete contextObj._requests;
 
   const reactApp = renderToString(
-    sheet.collectStyles(
+    <Provider value={contextObj}>
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
-    )
+    </Provider>
   );
 
   const templateFile = path.resolve('./build/index.html');
@@ -49,15 +68,12 @@ app.get('/*', (req, res) => {
     if (err) {
       return res.status(500).send(err);
     }
-
-    const loadedArticles = articles;
-
     return res.send(
       data
         .replace(
           '<div id="root"></div>',
-          `<script>window.preloadedArticles = ${JSON.stringify(
-            loadedArticles
+          `<script>window.preloadedData = ${JSON.stringify(
+            contextObj
           )}</script> <div id="root">${reactApp}</div>`
         )
         .replace('{{ styles }}', sheet.getStyleTags())
